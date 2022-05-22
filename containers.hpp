56a877aced59597f0bbe9f5cd1ba3db12dfc2e10
VLN37 <jofelipe@student.42sp.org.br>
2022-05-21 22:56:05 -0300

    misc: flesh out the instructions

---
+++ containers.hpp
@@ -0,0 +1,21 @@
+//Copyright (c) 2022 João Rodriguez A.K.A. VLN37. All rights reserved.
+//Creation date: 21/05/2022
+
+#ifndef CONTAINERS_HPP
+# define CONTAINERS_HPP
+
+#include <iostream>
+#include "vector.hpp"
+#include "map.hpp"
+#include "stack.hpp"
+#include "set.hpp"
+#include "rbtree.hpp"
+#include "iterator_base.hpp"
+#include "iterator_funcs.hpp"
+#include "iterator_tree.hpp"
+#include "iterator_vector.hpp"
+#include "algo.hpp"
+#include "pair.hpp"
+#include "type_traits.hpp"
+
+#endif // CONTAINERS_HPP
--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 //Copyright (c) 2022 João Rodriguez A.K.A. VLN37. All rights reserved.
 //Creation date: 20/05/2022
 
-#include <iostream>
+#include "containers.hpp"
 
-#include "vector.hpp"
-#include "map.hpp"
-#include "stack.hpp"
-#include "set.hpp"
-#include "rbtree.hpp"
-#include "iterator_base.hpp"
-#include "iterator_funcs.hpp"
-#include "iterator_tree.hpp"
-#include "iterator_vector.hpp"
-#include "algo.hpp"
-#include "pair.hpp"
-#include "type_traits.hpp"
+/**
+ * This project reimplements several std containers from scratch.
+ * you are able to use the following containers and all its functions:
+ * map
+ * set
+ * vector
+ * stack
+ * namespace is ft
+ * performance is comparable to std
+**/
 
 int main(void) {
   std::cout << "have fun with containers!\n";
+
+  //example
+  ft::map<int, std::string> map;
+
+  map.insert(ft::make_pair(42, std::string("42 is the answer")));
+  std::cout << map.find(42)->second << "\n";
 }
